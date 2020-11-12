0a4930d8bdb2064497ce02e82595738131256954
joao paulo <joaopaulopmedeiros@gmail.com>
2020-11-12 16:51:51 -0300

    Abandoning type cvli seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            RoleSeeder::class,
+            RoleSeeder::class
         ]);
     }
 }
