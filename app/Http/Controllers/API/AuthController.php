73035f880b595039fcf7ea9442f8f9bd591a0d95
joao paulo <joaopaulopmedeiros@gmail.com>
2020-11-18 16:22:11 -0300

    Fixing validation error display

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -63,7 +63,7 @@ class AuthController extends Controller
             ]);
 
             if($validator->fails()){
-                return response()->json($validator->errors()->toJson(), 400);
+                return response()->json(["error" => $validator->errors()->toJson()], 400);
             }
 
             DB::beginTransaction();
