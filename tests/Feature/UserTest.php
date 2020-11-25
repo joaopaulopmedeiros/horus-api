3d7ca1b9abbaee1a30abf310f55dbd11228342ed
joao paulo <joaopaulopmedeiros@gmail.com>
2020-11-25 01:43:11 -0300

    UserTest - checking delete, update and show methods

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -12,6 +12,7 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 class UserTest extends TestCase
 {
     protected $base_url = "http://localhost:8000/api";
+
     /**
      * @test
      * @return void
@@ -31,6 +32,52 @@ class UserTest extends TestCase
      * @test
      * @return void
      */
+    public function normal_user_should_not_delete_the_others_or_himself()
+    {
+        $user = $this->retrieveUser(2);
+
+        $token = JWTAuth::fromUser($user);
+
+        $randomUser = $this->retrieveUser(2);
+
+        $response = $this->delete(
+            "$this->base_url/users/{$randomUser->id}/?token={$token}",
+            []
+        );
+
+        $response->assertStatus(500)->assertJsonStructure(['error']);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function any_user_should_not_update_a_different_user()
+    {
+        $user = $this->retrieveUser(2);
+
+        $token = JWTAuth::fromUser($user);
+
+        $faker = \Faker\Factory::create();
+
+        $randomUser = $this->retrieveUser(2);
+
+        while($randomUser->id == $user->id) {
+            $randomUser = $this->retrieveUser(2);
+        }
+
+        $response = $this->put(
+            "$this->base_url/users/{$randomUser->id}/?token={$token}",
+            ['email' => $faker->unique()->safeEmail]
+        );
+
+        $response->assertStatus(500)->assertJsonStructure(['error']);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
     public function normal_user_should_see_only_himself()
     {
         $user = $this->retrieveUser(2);
@@ -42,7 +89,7 @@ class UserTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function retrieveUser($role)
+    public function retrieveUser(int $role): User
     {
         return User::whereHas('role', function(Builder $query) use ($role) {
             $query->where('users_roles.roles_id', '=', $role);
