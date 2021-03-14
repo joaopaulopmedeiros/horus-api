67e035b0032e0ca1305e96ce60aba142eb75a95c
João Paulo <jpaulope@indracompany.com>
2021-03-14 18:51:01 -0300

    feat: changing from starts to verified

--- database/seeders/CvliSeeder.php
+++ database/seeders/CvliSeeder.php
@@ -18,20 +18,20 @@ class CvliSeeder extends Seeder
     {
         $cvlis = [
             [
-                'title' => 'Crime 01',
-                'description' => 'Lorem Ipsum is simply text of the printing and typesetting industry.',
+                'title' => 'Roubo de veículo',
+                'description' => 'Ocorrido à luz do dia na Zona Oeste. Veículo celta preto.',
                 'latitude' => -5.7490999,
                 'longitude' => -35.262599,
-                'stars' => 1,
+                'verified' => true,
                 'cvli_type_id' => TypeCvli::first()->id,
                 'user_id' => User::first()->id
             ],
             [
-                'title' => 'Crime 02',
-                'description' => 'Lorem Ipsum is simply text of the printing and typesetting industry.',
+                'title' => 'Assalto à mão armada',
+                'description' => 'Dois assaltantes roubaram estudantes por volta das 8h no Planalto.',
                 'latitude' => -5.8450935,
                 'longitude' => -35.2697694,
-                'stars' => 2,
+                'verified' => true,
                 'cvli_type_id' => TypeCvli::first()->id,
                 'user_id' => User::first()->id
             ]
