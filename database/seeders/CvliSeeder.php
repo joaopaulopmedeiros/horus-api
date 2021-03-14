ee41abd781b8def6e43319abbc4810d5355fc1d2
João Paulo <jpaulope@indracompany.com>
2021-03-14 17:18:03 -0300

    feat: Cvli seeder

--- database/seeders/CvliSeeder.php
+++ database/seeders/CvliSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Cvli;
+use App\Models\TypeCvli;
+use App\Models\User;
 
 class CvliSeeder extends Seeder
 {
@@ -14,24 +16,26 @@ class CvliSeeder extends Seeder
      */
     public function run()
     {
-        Cvli::create([
-            'title' => 'Crime 01',
-            'latitude' => 1,
-            'longitude' => 2,
-            'stars' => 1,
-            'cvli_type_id' => 1,
-            'user_id' => 1
-        ]);
-
-        /*
-                    $table->id();
-            $table->float('latitude', 12, 10);
-            $table->float('longitude', 12, 10);
-            $table->float('stars', 3, 2)->default(1);
-            $table->timestamps();
-
-            $table->unsignedBigInteger('cvli_type_id');
-            $table->foreign('cvli_type_id')->references('id')->on('cvlis_types');
-        */
+        $cvlis = [
+            [
+                'title' => 'Crime 01',
+                'latitude' => -5.7490999,
+                'longitude' => -35.262599,
+                'stars' => 1,
+                'cvli_type_id' => TypeCvli::first()->id,
+                'user_id' => User::first()->id
+            ],
+            [
+                'title' => 'Crime 02',
+                'latitude' => -5.8450935,
+                'longitude' => -35.2697694,
+                'stars' => 2,
+                'cvli_type_id' => TypeCvli::first()->id,
+                'user_id' => User::first()->id
+            ]
+        ];
+        foreach ($cvlis as $cvli){
+            Cvli::create($cvli);
+        }
     }
 }
